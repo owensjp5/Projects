8039117df8ae5c32b39ee23af8190bb992bc080b
owensjp5 <jackowens@Jacks-MacBook-Pro.local>
2025-04-21 09:02:52 -0700

    Added Steady State prediction functions

--- Computational-Biology/Central-Dogma.py
+++ Computational-Biology/Central-Dogma.py
@@ -17,6 +17,17 @@ def simulate(variables, t, params):
 
     return ([dmdt, dpdt])
     
+def getSteadyState(params):
+    k_m = params[0]
+    gamma_m = params[1]
+    k_p = params[2]
+    gamma_p = params[3]
+
+    m_SS = k_m/gamma_m
+    p_SS = (k_p * m_SS) / gamma_p
+    
+    return m_SS, p_SS
+
 def main():
    #Initialize Variables
     y0 = [0,0]
@@ -29,13 +40,15 @@ def main():
 
     rates = [k_m, gamma_m, k_p, gamma_p]
 
+    print("Steady States:", getSteadyState(rates))
+
     #Run Simulation
     y = odeint(simulate, y0, t, args=(rates,))
     
     #Plot Results
     f, ax = plt.subplots(1)
-    line1, = ax.plot(t,y[:,0], color='b',label="Gene 1")
-    line2, = ax.plot(t,y[:,1], color='r',label="Gene 2")
+    line1, = ax.plot(t,y[:,0], color='b',label="mRNA")
+    line2, = ax.plot(t,y[:,1], color='r',label="Protein")
 
     ax.set_ylabel("Quantity")
     ax.set_xlabel("Time")
