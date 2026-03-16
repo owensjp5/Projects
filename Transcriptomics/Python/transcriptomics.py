ab52416a8bbb75aedfa834285885fc1ef502d2d9
owensjp5 <owensjp5@gmail.com>
2026-03-16 10:17:32 -0500

    Start DEG analysis in python

--- Transcriptomics/Python/transcriptomics.py
+++ Transcriptomics/Python/transcriptomics.py
@@ -7,7 +7,6 @@ from scipy.stats import ks_2samp
 import seaborn as sns
 
 # read in gene expression data
-
 df = pd.read_csv('/Users/jackowens/desktop/Projects/Transcriptomics/ref/GSE150910_gene-level_count_file.csv')
 
 genes = []
@@ -30,19 +29,16 @@ for i in range(len(samples)):
 labels = np.array(labels)
 
 # remove chp samples
-
 data = data[:, labels != "chp"]
 samples = samples[labels != "chp"]
 labels = labels[labels != "chp"]
 
 # normalize data (counts per million)
-
 for j in range(data.shape[1]):
     column_sum = sum(data[:,j])
     data[:,j] = data[:,j] / column_sum * 10**6
 
 # filter out low-expression genes
-
 mean_CPM_control = data[:,labels == "control"].mean(axis=1)
 mean_CPM_ipf = data[:,labels == "ipf"].mean(axis=1)
 
@@ -50,3 +46,44 @@ to_keep = (mean_CPM_ipf >= 5) | (mean_CPM_control >= 5)
 
 data = data[to_keep, :]
 genes = genes[to_keep]
+
+# K-S Tests evaluate likelihood that two sample distributions were drawn from the same distribution
+
+p_values = []
+log2_FCs = []
+
+for i in range(data.shape[0]):
+    control = data[i, labels == "control"]
+    ipf = data[i, labels == "ipf"]
+
+    ks_statistic, p_value = ks_2samp(control, ipf)
+
+    p_values.append(p_value)
+
+    epsilon = 1
+    fold_change = (np.mean(ipf) + epsilon) / (np.mean(control) + epsilon)
+    log2_FC = np.log2(fold_change)
+
+    log2_FCs.append(log2_FC)
+
+p_values = np.array(p_values)
+log2_FCs = np.array(log2_FCs)
+
+# Bonferroni, multiple test correction
+p_values_bonf = p_values * len(genes)
+bonf_threshold = 0.05
+# threshold = 0.05 / len(genes)
+to_keep = (p_values_bonf <= bonf_threshold) & (np.abs(log2_FCs) >= 1)
+# to_keep = (p_values <= threshold) & (np.abs(log2_FCs) >= 1) #Alternate way to perform bonf correction
+
+sig_genes = genes[to_keep]
+sig_log2_FCs = log2_FCs[to_keep]
+sig_p_values = p_values_bonf[to_keep]
+sig_data = data[to_keep, :]
+
+print("Num DEGs:", len(sig_genes))
+print("Total Genes:", len(genes))
+print("DEG Percentage:", len(sig_genes) / len(genes) * 100)
+print()
+print("Number of upregulated DEGs:", np.sum(sig_log2_FCs>0))
+print("Number of downregulated DEGs:", np.sum(sig_log2_FCs<0))
