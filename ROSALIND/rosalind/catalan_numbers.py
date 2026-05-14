cda2d415f89f57c8c88e3e01825ce004cae303af
owensjp5 <owensjp5@gmail.com>
2026-05-14 14:54:27 -0500

    Solve Catalan Numbers/RNA Secondary Structure

---
+++ ROSALIND/rosalind/catalan_numbers.py
@@ -0,0 +1,37 @@
+################################################
+# Catalan Numbers and RNA Secondary Structures #
+################################################
+import numpy as np
+
+def catalan_number(seq):
+    complementBase = {"A":"U","U":"A","C":"G","G":"C"}
+    n = len(seq)
+    dp = np.zeros(shape=(n, n), dtype=object)
+
+    for i in range(n-1):
+        if complementBase[seq[i]] == seq[i+1]:
+            dp[i, i+1] = 1
+
+    for j in range(3, n, 2):
+        for i in range(n-j):
+            for k in range(i+1, i+j+1, 2):
+                if complementBase[seq[i]] == seq[k]:
+                    if k > i+1:
+                        left  = dp[i+1, k-1]
+                    else:
+                        left = 1
+                    if k < i+j:
+                        right = dp[k+1, i+j]
+                    else:
+                        right = 1
+                    dp[i, i+j] = (dp[i, i+j] + left * right) % 1000000
+
+    return int(dp[0, n-1])
+
+def main():
+    print(catalan_number("CGAUCG"))
+    print(catalan_number("AUAUAU"))
+    print(catalan_number("GUUUUACACGCGUGAUAUAAUCGUGUACAUGUCGUUAAACAUAAAUCGGCAUGGCCAAUAUCGGCUCAAUUGCCAAUUGGUAGGCGCUUAAUACCGACAUGUGUGAUGCGCCAAAGCUGCUACGGCUCGAUGCAUCGUAUAUAUGCGCAGCCGAUAUCCGCGUGCGCAUAACGGUCCGCUAGGAUAGCGCGCUACCAUACGUAUAGGCCAGCUUAGCUCGCGGAUCUAUGCAAUUAUAUAUCGCAUGCUAUAUAGUAUAGCAGGCCUAGCCAUG"))
+
+if __name__ == "__main__":
+    main()
