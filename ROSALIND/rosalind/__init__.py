68bcf5a97ffa5d114031a87fdbbaaf0a538981c6
owensjp5 <owensjp5@gmail.com>
2026-05-15 15:25:07 -0500

    Solve Error Correction in Reads

--- ROSALIND/rosalind/__init__.py
+++ ROSALIND/rosalind/__init__.py
@@ -31,3 +31,4 @@ from .construct_tree import connectEdges
 from .count_subsets import totalSubsets
 from .random_strings import randomString
 from .catalan_numbers import catalanNumber
+from .error_correction import correctPointMutations
