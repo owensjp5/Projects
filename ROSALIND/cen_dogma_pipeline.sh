dbe3f54755a97bfd7d1bc84b1847c96467298b7a
owensjp5 <owensjp5@gmail.com>
2026-05-28 10:36:32 -0500

    Redirect pipeline Stdoutput

--- ROSALIND/cen_dogma_pipeline.sh
+++ ROSALIND/cen_dogma_pipeline.sh
@@ -4,5 +4,6 @@ inputFilePath=$1
 outdir=$2
 
 mkdir -p pipeline_outputs/$outdir
+touch pipeline_outputs/$outdir/output.txt
 
 python3 central_dogma.py $inputFilePath $outdir
--- ROSALIND/central_dogma.py
+++ ROSALIND/central_dogma.py
@@ -4,13 +4,14 @@ import rosalind
 import sys
 
 numArgs = len(sys.argv)
-if numArgs <= 2:
+if numArgs < 2:
     sys.exit("Error: Missing first command line argument: input file")
-if numArgs != 3:
+if numArgs < 3:
     sys.exit("Error: Missing second command line argument: output directory")
 
 genomeFile = sys.argv[1]
 outdir = sys.argv[2]
+outputFilePath = "pipeline_outputs/" + outdir + "/output.txt"
 
 DNA_seqs = rosalind.readSequencesFromFasta(genomeFile)
 
@@ -22,12 +23,16 @@ for key, RNA_seq in RNA_seqs.items():
     if ORFs:
         PROT_seqs[key] = ORFs
 
-for key in DNA_seqs:
-    print(key)
-    print(DNA_seqs[key])
-    print(RNA_seqs[key])
-    try:
-        print(PROT_seqs[key])
-    except:
-        print("'No ORFs'")
-    print()
+with open(outputFilePath, 'w') as outputFile:
+    prevStdout = sys.stdout
+    sys.stdout = outputFile
+    for key in DNA_seqs:
+        print(key)
+        print(DNA_seqs[key])
+        print(RNA_seqs[key])
+        try:
+            print(PROT_seqs[key])
+        except:
+            print("'No ORFs'")
+        print()
+    sys.stdout = prevStdout
